8ae5553417af8d2b4c2c1c84aadda37960f7fd11
MardanovTimur <alisa030897@gmail.com>
2016-10-05 15:24:16 +0300

    Early development.

---
+++ oct_project/src/main/java/ru/itis/inform/models/User.java
@@ -0,0 +1,11 @@
+package ru.itis.inform.models;
+
+/**
+ * Created by Тимур on 03.10.2016.
+ */
+public class User {
+    private int id;
+    private char name;
+    private char[] login;
+    private char[] password;
+}
