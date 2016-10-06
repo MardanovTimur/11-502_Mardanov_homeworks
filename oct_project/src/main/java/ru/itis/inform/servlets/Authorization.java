bcdfd7fa3ce5e5d65969aec4d2e648d235033de2
MardanovTimur <alisa030897@gmail.com>
2016-10-06 18:09:08 +0300

    Add web.xml, index.jsp, Authorization servlet

---
+++ oct_project/src/main/java/ru/itis/inform/servlets/Authorization.java
@@ -0,0 +1,22 @@
+package ru.itis.inform.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Тимур on 06.10.2016.
+ */
+public class Authorization extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
